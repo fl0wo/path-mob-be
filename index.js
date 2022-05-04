f9ba3597fca00983fd88950077fa24dd00c1c1bb
Florian Sabani <fsabani00@gmail.com>
2022-05-04 10:48:55 +0200

    Now introducing api for taxi retrival also

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 var cors = require('cors')
+var request = require('request');
+const axios = require('axios');
+const bodyParser = require('body-parser')
 
 const rotte = require("./routesBus.json"); // Fake httpcall
 
 const app = express();
 
 app.use(cors())
+
+app.use(express.static('client'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.get("/bus/all", (req, res, next) => {
 
@@ -23,6 +30,42 @@ app.get("/bus/all", (req, res, next) => {
 });
 
 
-app.listen(3001, () => {
+
+async function getTaxis(lat, long) {
+
+    return await axios.post(
+        'http://demo.data.wetaxi.org/v5/partners/estimates',
+        // '{\n    "departure": {\n        "latitude":45.0614548,\n        "longitude":7.6768899\n    },\n    "passengers": 2,\n    "asap":true\n}',
+        {
+            departure: {
+                latitude: 45.0614548,
+                longitude: 7.6768899
+            },
+            passengers: 2,
+            asap: true
+        },
+        {
+            headers: {
+                'Authorization': 'Bearer 2Cu9cnQ7wYr8vsZqQfXmqdRsS9rsRLz0MS2Y6SFIwmyKmTMhPQDxnGG7MUX0suMpVl2Ox8dWqU0TIysiKiLgRHn6bZSQkjqsdGgbgDJclsRnNr7BfxgREsVIE2NkQ3gaAnZFxFmiwyc8EINR4pWur27W4orGOS1CATNUQDKW1mGULTdqd3P2yRBfoEAdZ08IccidZjAfgSJVKYde6wu2YDb8n0E0KUxeedEOdgyjnGVuNcyU1UENQSyLWCpSxFtB',
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+}
+
+app.get("/taxi/all", async (req, res, next) => {
+
+    let lat = req.headers['lat']
+    let long = req.headers['long']
+
+    let taxies = (await getTaxis(lat, long)).data;
+
+    console.log(taxies)
+
+    return res.json(taxies)
+});
+
+
+app.listen(3401, () => {
     console.log("Server running on port 3001");
 });
